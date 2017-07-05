d26efe39795da76ac389c3c91551fd7b41ef0ef0
Pratik Pawar <pratikppawar@users.noreply.github.com>
2017-07-05 14:14:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,33 +2,52 @@ from flask import Flask, render_template
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
+import json
+import logging
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Jarvis")
+bot = ChatBot("Adam",
+    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.65,
+            'default_response': 'I am sorry, but I do not understand. Please rephrase the question'
+        },
+        {
+            'import_path': 'chatterbot.logic.MathematicalEvaluation'
+        }
+    ],
+    filters=[
+        'chatterbot.filters.RepetitiveResponseFilter'
+    ],
+    trainer="chatterbot.trainers.ListTrainer",
+    database="../database.db"
+)
 
 
-"""
-english_bot.set_trainer(ListTrainer)
-english_bot.train([
-    "Hi there!",
-    "Hello",
-])
-english_bot.train([
-    "Greetings!",
-    "Hello",
-])
-english_bot.train([
+with open('statements.json') as json_data:
+    data = json.load(json_data)
+    #print(d.get("statements"))
+for item in data:
+    bot.train(item)
+
+bot.train([
     "How are you?",
     "I am good.",
     "That is good to hear.",
     "Thank you",
-    "You are welcome.",
+    "You are welcome. How can I help you?",
 ])
-"""
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english.greetings",
-    "chatterbot.corpus.english.conversations")
+
+bot.train(
+    "chatterbot.corpus.english.greetings"
+)
+
 
 @app.route("/")
 def home():
@@ -36,7 +55,7 @@ def home():
 
 @app.route("/get/<string:query>")
 def get_raw_response(query):
-    return str(english_bot.get_response(query))
+    return str(bot.get_response(query))
 
 
 if __name__ == "__main__":
